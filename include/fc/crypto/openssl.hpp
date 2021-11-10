a1974c6e0ab21b8aca8bc81a520791a7a96f774c
Huang-Ming Huang <huangh@objectcomputing.com>
2021-11-10 09:13:17 -0600

    remove openssl 1.0 support

--- include/fc/crypto/openssl.hpp
+++ include/fc/crypto/openssl.hpp
@@ -61,13 +61,6 @@ namespace fc
         ~ssl_bignum() { BN_free(obj); }
     };
 
-    /** Allows to explicitly specify OpenSSL configuration file path to be loaded at OpenSSL library init.
-        If not set OpenSSL will try to load the conf. file (openssl.cnf) from the path it was
-        configured with what caused serious Keyhotee startup bugs on some Win7, Win8 machines.
-        \warning to be effective this method should be used before any part using OpenSSL, especially
-        before init_openssl call
-    */
-    void store_configuration_path(const path& filePath);
     int init_openssl();
 
 } // namespace fc
--- src/crypto/openssl.cpp
+++ src/crypto/openssl.cpp
@@ -8,75 +8,11 @@
 #include <string>
 #include <stdlib.h>
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-void ECDSA_SIG_get0(const ECDSA_SIG *sig, const BIGNUM **pr, const BIGNUM **ps) {
-   if (pr != NULL)
-      *pr = sig->r;
-   if (ps != NULL)
-      *ps = sig->s;
- }
-
- int ECDSA_SIG_set0(ECDSA_SIG *sig, BIGNUM *r, BIGNUM *s) {
-   if (r == NULL || s == NULL)
-      return 0;
-   BN_clear_free(sig->r);
-   BN_clear_free(sig->s);
-   sig->r = r;
-   sig->s = s;
-   return 1;
-}
-#endif
-
 namespace  fc 
 {
-    struct openssl_scope
-    {
-       static path& config_path() {
-          static path cfg;
-          return cfg;
-       }
-       openssl_scope()
-       {
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-          ERR_load_crypto_strings(); 
-          OpenSSL_add_all_algorithms();
-#endif
-          const boost::filesystem::path& boostPath = config_path();
-          if(boostPath.empty() == false)
-          {
-            std::string varSetting("OPENSSL_CONF=");
-            varSetting += config_path().to_native_ansi_path();
-#if defined(WIN32)
-            _putenv((char*)varSetting.c_str());
-#else
-            putenv((char*)varSetting.c_str());
-#endif
-          }
-
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
- 	       OPENSSL_config(nullptr);
-#else
-	       OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CONFIG, NULL);
-#endif
-       }
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-       ~openssl_scope()
-       {
-          EVP_cleanup();
-          ERR_free_strings();
-       }
-#endif           
-    };
-
-
-    void store_configuration_path(const path& filePath)
-    {
-      openssl_scope::config_path() = filePath;
-    }
-   
     int init_openssl()
     {
-      static openssl_scope ossl;
+      OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CONFIG, NULL);
       return 0;
     }
 }
